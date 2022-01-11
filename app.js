2aad678d755f9aa925bdf962b2345b55a478c645
leovenom <leonardt@outlook.com>
2022-01-11 12:01:54 +0100

    added function draw, undraw and freeze

--- app.js
+++ app.js
@@ -44,15 +44,46 @@ document.addEventListener('DOMContentLoaded',() => {
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
     let currentPosition = 4;
-    let current = theTetrominoes[0][0];
+    let currentRotation = 0;
+    //randomly select a Tetromino and its first rotation
+    let random = Math.floor(Math.random()*theTetrominoes.length)
+    let current = theTetrominoes[random][currentRotation];
 
-    //draw the first rotation in the first tetromino
-
+    //draw the Tetromino
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino')
         });
     }
 
-    draw();
+    //undraw the Tetromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        });
+    }
+
+    //make the Teromino move down every second
+    timeId = setInterval(moveDown, 1000);
+
+    //move down function
+    function moveDown() {
+        undraw();
+            currentPosition += width;
+        draw();
+        freeze();
+    }
+
+    //freeze function
+    function freeze(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        //start a new Tetromino falling
+        random = Math.floor(Math.random()*theTetrominoes.length);
+        current = theTetrominoes[random][currentRotation];
+        currentPosition = 4;
+        draw();
+        }
+    }
+
 })
